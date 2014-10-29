3976e5c55aa63d3c2be0ef170767fc3c2ac68b4b
Jerel Unruh <mail@unruhdesigns.com>
2014-10-29 17:56:58 -0500

    Updated production settings

--- morgan/settings/prod.py
+++ morgan/settings/prod.py
@@ -1,15 +1,18 @@
-
+import os
 from morgan.settings import *   # pylint: disable=W0614,W0401
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'morgan',
-#        'USER': 'dbuser',
-#        'PASSWORD': 'dbpassword',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
+
+ALLOWED_HOSTS = ('dev.morgan-911.org', 'morgan-county.org',)
 
