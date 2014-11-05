2f81a95a9a23b63dd4461392d016d18f1df1bb8c
Jerel Unruh <mail@unruhdesigns.com>
2014-11-05 11:07:33 -0600

    Added email settings

--- morgan/settings/prod.py
+++ morgan/settings/prod.py
@@ -18,3 +18,12 @@ DATABASES = {
 
 ALLOWED_HOSTS = ('dev.morgan-911.org', 'morgan-county.org',)
 
+DEFAULT_FROM_EMAIL = 'Morgan County <{0}>'.format(os.environ.get('EMAIL_FROM'))
+SERVER_EMAIL = os.environ.get('EMAIL_FROM')
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_PORT = 587
+EMAIL_USE_TLS = True
+
